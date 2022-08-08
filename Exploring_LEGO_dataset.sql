84f2a0ba4687aee85daaf4303639aea9ae0a4153
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-08-08 21:17:34 +0600

    Update Exploring_LEGO_dataset.sql

--- Exploring_LEGO_dataset.sql
+++ Exploring_LEGO_dataset.sql
@@ -4,15 +4,32 @@
 
 SELECT s.set_num, s.name AS set_name, s.year, s.theme_id,
        CAST(s.num_parts AS numeric), t.name AS theme_name, t.parent_id,
-       p.name AS parent_theme_name
+       p.name AS parent_theme_name,
+	   CASE WHEN s.year BETWEEN 1901 AND 2000 THEN '20th Century'
+	   		WHEN s.year BETWEEN 2001 AND 2100 THEN '21st Century'
+			END AS century
 INTO analytics_main
 FROM sets AS s
 LEFT JOIN themes AS t
     ON s.theme_id = t.id
 LEFT JOIN themes AS p
     ON p.parent_id = p.id;
+	
 
--- What is the total number of parts per theme
+-- 1. What is the total number of parts per theme?
 
+SELECT theme_name, sum(num_parts) AS total_num_parts
+FROM analytics_main
+WHERE parent_theme_name IS NOT NULL
+GROUP BY theme_name
+ORDER BY total_num_parts DESC;
 
+-------------------------------------------------------------------
 
+-- 2. What is the total number of parts per year?
+
+SELECT year, sum(num_parts) AS total_num_parts
+FROM analytics_main
+WHERE parent_theme_name IS NOT NULL
+GROUP BY year
+ORDER BY total_num_parts DESC;
