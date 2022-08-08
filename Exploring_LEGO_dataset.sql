4956ecf9de1ffbe1521ac5916df5b005078f3aa1
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-08-08 22:32:12 +0600

    Update Exploring_LEGO_dataset.sql

--- Exploring_LEGO_dataset.sql
+++ Exploring_LEGO_dataset.sql
@@ -47,7 +47,7 @@ GROUP BY century;
 
 ---------------------------------------------------------------------
 
--- 4. --- What percentage of sets ever released in the 21st century were trains themed? 
+-- 4. What percentage of sets ever released in the 21st century were trains themed? 
 
 WITH cte AS 
 (
@@ -58,7 +58,8 @@ WITH cte AS
 )
 SELECT SUM(total_set_num), SUM(percentage)
 FROM(
-	SELECT Century, theme_name, total_set_num, SUM(total_set_num) OVER() AS total,  cast(1.00 * total_set_num / sum(total_set_num) OVER() AS decimal(5,4))*100 Percentage
+	SELECT Century, theme_name, total_set_num, SUM(total_set_num) OVER() AS total, 
+    cast(1.00 * total_set_num / sum(total_set_num) OVER() AS decimal(5,4))*100 Percentage
 	FROM cte	
 	--order by 3 desc
 	)m
@@ -68,7 +69,45 @@ WHERE theme_name LIKE '%Star wars%';
 
 -----------------------------------------------------------------------
 
+-- 5. What was the popular theme by year in terms of sets released in the 21st Century?
+SELECT year, theme_name, total_set_num
+FROM (
+	SELECT year, theme_name, 
+    COUNT(set_num) total_set_num, ROW_NUMBER() OVER (PARTITION BY year ORDER BY COUNT(set_num) DESC) rn
+    
+	FROM analytics_main
+	WHERE century = '21st Century'
+		--and parent_theme_name is not null
+	GROUP BY year, theme_name
+)m
+WHERE rn = 1	
+ORDER BY year DESC;
 
+---------------------------------------------------------------------------------
+
+
+-- 6. What is the most produced color of lego ever in terms of quantity of parts?
+
+SELECT color_name, sum(quantity) AS quantity_of_parts
+FROM 
+	(
+		SELECT
+			inv.color_id, inv.inventory_id, inv.part_num, 
+            CAST(inv.quantity AS numeric) quantity, inv.is_spare, 
+            c.name AS color_name, c.rgb, p.name AS part_name, 
+            p.part_material, pc.name AS category_name
+
+		FROM inventory_parts inv
+		INNER JOIN colors c
+			ON inv.color_id = c.id
+		INNER JOIN parts p
+			ON inv.part_num = p.part_num
+		INNER JOIN part_categories pc
+			ON part_cat_id = pc.id
+	)main
+
+GROUP BY color_name
+ORDER BY 2 DESC;
 
 
 
