d14c0411e0f8a59e3a682da2f4ca7ae3195ff8c1
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-08-08 21:06:41 +0600

    Update Exploring_LEGO_dataset.sql

--- Exploring_LEGO_dataset.sql
+++ Exploring_LEGO_dataset.sql
@@ -1,12 +1,18 @@
 -- EXPLORING LEGO DATASET
 
--- What is the total number of parts per theme
+-- Joining two datasets sets and themes for the analysis purposes
 
-SELECT s.set_num, s.name, s.year, s.theme_id,
-       s.num_parts, t.name AS theme_name, t.parent_id,
+SELECT s.set_num, s.name AS set_name, s.year, s.theme_id,
+       CAST(s.num_parts AS numeric), t.name AS theme_name, t.parent_id,
        p.name AS parent_theme_name
+INTO analytics_main
 FROM sets AS s
 LEFT JOIN themes AS t
     ON s.theme_id = t.id
 LEFT JOIN themes AS p
-    ON p.parent_id = p.id
+    ON p.parent_id = p.id;
+
+-- What is the total number of parts per theme
+
+
+
