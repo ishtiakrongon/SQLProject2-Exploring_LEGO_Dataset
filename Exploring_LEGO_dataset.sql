b2fbf95b90b02ec4b30d9807b50f47f0a79a7b94
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-08-08 20:12:44 +0600

    1st query

--- Exploring_LEGO_dataset.sql
+++ Exploring_LEGO_dataset.sql
@@ -1,3 +1,12 @@
 -- EXPLORING LEGO DATASET
 
 -- What is the total number of parts per theme
+
+SELECT s.set_num, s.name, s.year, s.theme_id,
+       s.num_parts, t.name AS theme_name, t.parent_id,
+       p.name AS parent_theme_name
+FROM sets AS s
+LEFT JOIN themes AS t
+    ON s.theme_id = t.id
+LEFT JOIN themes AS p
+    ON p.parent_id = p.id
